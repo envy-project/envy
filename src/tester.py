b615b28889d2a35561f532b0a201f028c2f13164
Alex Roth <alex@magmastone.net>
2019-06-06 23:15:26 -0400

    Add tester

--- src/tester.py
+++ src/tester.py
@@ -1,4 +1,9 @@
 import docker
 from lib.docker_helpers.container_finder import ContainerFinder
+import dockerpty
+
 CLIENT = docker.from_env()
 T = ContainerFinder(CLIENT)
+CONTAINER = T.findAndEnsureRunning()
+
+dockerpty.exec_command(CLIENT, CONTAINER.id, '/bin/bash')
