0143c1fef2add921be508fb71ca9eb1b41f13fe0
omstrumpf <omstrumpf@gmail.com>
2019-06-07 21:12:56 -0400

    validating dependencies config

--- src/lib/config/validate.py
+++ src/lib/config/validate.py
@@ -1,12 +1,23 @@
 import sys
-from schema import Schema, SchemaError, Optional
+from schema import Schema, SchemaError, Optional, Or
 
 # TODO add schema information for environment stuff
 _SCHEMA = Schema(
     {
+        Optional("environment"): {
+            Optional("dependencies"): {
+                Optional("python2"): str,
+                Optional("python3"): str,
+                Optional("node"): str,
+                Optional("ruby"): str,
+                Optional("native"): [
+                    {"recipe": str, "version": Or(str, int, float)},
+                ],
+            },
+        },
         Optional("actions"): [
-            {"name": str, "script": str, "help": str, Optional("description"): str}
-        ]
+            {"name": str, "script": str, "help": str, Optional("description"): str},
+        ],
     }
 )
 
