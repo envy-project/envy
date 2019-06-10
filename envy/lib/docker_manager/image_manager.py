c0b451fc5bcedb4b331fbd814908c8399f2ecc79
Omer Strumpf <omstrumpf@gmail.com>
2019-06-10 17:46:52 -0400

    type hints and docstrings for docker manager (#1)
    * docstrings and type hints for docker manager
    
    * fixing conflict between pylint and black

--- envy/lib/docker_manager/image_manager.py
+++ envy/lib/docker_manager/image_manager.py
@@ -1,3 +1,6 @@
+from docker import DockerClient
+from docker.models.images import Image
+
 from envy.lib.config import ENVY_CONFIG
 from envy.lib.file_downloader import resolve_files
 
@@ -7,7 +10,15 @@ from .image_creators import AptImageCreator
 class ImageManager:
     ### Static Image Creation ###
     @staticmethod
-    def create(docker_client):
+    def create(docker_client: DockerClient) -> "ImageManager":
+        """ Creates an image from the ENVY_CONFIG environment
+
+        Arguments:
+            docker_client {DockerClient} -- A docker client
+
+        Returns:
+            [ImageManager] -- an image manager for the created image
+        """
         print("Creating ENVy environment image.")
 
         # TODO: Use the correct image creator based on the config file!
@@ -19,17 +30,28 @@ class ImageManager:
         return ImageManager(docker_client, image_id)
 
     ### Image Management ###
-    def __init__(self, docker_client, image_id):
+    def __init__(self, docker_client: DockerClient, image_id: str) -> ImageManager:
+        """ Creates an image manager for the given image id
+
+        Arguments:
+            docker_client {DockerClient} -- A docker client
+            image_id {str} -- the image ID
+
+        Returns:
+            ImageManager -- an image manager for the created image
+        """
         self.docker_client = docker_client
 
         self.image_id = image_id
 
-    def __find(self):
+    def __find(self) -> Image:
         for image in self.docker_client.images.list():
             if image.id == self.image_id:
                 return image
         return None
 
     def destroy(self):
+        """ Destroys the image
+        """
         if self.image_id and self.__find():
             self.docker_client.images.remove(self.image_id)
