179e78e9d82554c2757b219d94bb04d3517a983b
omstrumpf <omstrumpf@gmail.com>
2019-06-19 09:42:03 -0400

    basic error handling

--- envy/lib/docker_manager/compose_manager.py
+++ envy/lib/docker_manager/compose_manager.py
@@ -1,5 +1,5 @@
 import subprocess
-from subprocess import DEVNULL
+from subprocess import CalledProcessError
 
 
 class ComposeManager:
@@ -9,28 +9,64 @@ class ComposeManager:
     def __init__(self, compose_file: str):
         self.compose_file = compose_file
 
-    # TODO compose may not be installed, should curl for it
-    # TODO check subprocess outputs for error
+    # TODO compose may not be installed
     # TODO I don't think we can do anything about this, but we *will* leave images dangling here. Probably just something we're ok with.
 
     def up(self):
         # TODO detect that pull is necessary and print something, since it can take a while
-        subprocess.check_output(
-            ["docker-compose", "-f", self.compose_file, "pull"], stderr=DEVNULL
-        )
-        subprocess.check_output(
-            ["docker-compose", "-f", self.compose_file, "up", "-d"], stderr=DEVNULL
-        )
+        try:
+            subprocess.run(
+                ["docker-compose", "-f", self.compose_file, "pull"],
+                check=True,
+                capture_output=True,
+            )
+            subprocess.run(
+                ["docker-compose", "-f", self.compose_file, "up", "-d"],
+                check=True,
+                capture_output=True,
+            )
+        except CalledProcessError as e:
+            print(
+                "Failed to start sidecar services. Command returned with error code {}".format(
+                    e.returncode
+                )
+            )
+            print(e.stdout.decode())
+            print(e.stderr.decode())
 
     def down(self):
-        subprocess.check_output(
-            ["docker-compose", "-f", self.compose_file, "stop"], stderr=DEVNULL
-        )
+        try:
+            subprocess.run(
+                ["docker-compose", "-f", self.compose_file, "stop"],
+                check=True,
+                capture_output=True,
+            )
+        except CalledProcessError as e:
+            print(
+                "Failed to stop sidecar services. Command returned with error code {}".format(
+                    e.returncode
+                )
+            )
+            print(e.stdout.decode())
+            print(e.stderr.decode())
 
     def nuke(self):
-        subprocess.check_output(
-            ["docker-compose", "-f", self.compose_file, "kill"], stderr=DEVNULL
-        )
-        subprocess.check_output(
-            ["docker-compose", "-f", self.compose_file, "rm", "-sf"], stderr=DEVNULL
-        )
+        try:
+            subprocess.run(
+                ["docker-compose", "-f", self.compose_file, "kill"],
+                check=True,
+                capture_output=True,
+            )
+            subprocess.run(
+                ["docker-compose", "-f", self.compose_file, "rm", "-sf"],
+                check=True,
+                capture_output=True,
+            )
+        except CalledProcessError as e:
+            print(
+                "Failed to nuke sidecar services. Command returned with error code {}".format(
+                    e.returncode
+                )
+            )
+            print(e.stdout.decode())
+            print(e.stderr.decode())
