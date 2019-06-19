d382e98919a3b2143615bb767e1e36ccab8c925b
Greg Bateman <gerbot150@gmail.com>
2019-06-19 19:17:00 -0400

    Change envy state to be one json object rather than multiple files

--- envy/lib/state/envy_state.py
+++ envy/lib/state/envy_state.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 
@@ -14,6 +15,7 @@ class EnvyState:
 
     def __init__(self, dir_path):
         self.directory = dir_path
+        self.state = None
 
     def nuke(self):
         """ Removes the state directory
@@ -32,19 +34,10 @@ class EnvyState:
         return ENVY_CONFIG.get_environment_hash() != self.get_environment_hash()
 
     def get_environment_hash(self) -> str:
-        path = self.__get_environment_file()
-
-        if os.path.isfile(path):
-            with open(path, "r") as f:
-                return f.read().rstrip()
-        else:
-            return None
+        self.__get(['environment', 'md5'])
 
     def set_environment_hash(self, new_hash):
-        path = self.__get_environment_file()
-
-        with open(path, "w") as f:
-            f.write(new_hash)
+        self.__set(['environment', 'md5'], new_hash)
 
     def update_environment_hash(self):
         self.set_environment_hash(ENVY_CONFIG.get_environment_hash())
@@ -53,37 +46,65 @@ class EnvyState:
         return "{}/environment.md5".format(self.directory)
 
     def get_container_id(self):
-        path = self.__get_container_file()
-
-        if os.path.isfile(path):
-            with open(path, "r") as f:
-                return f.read().rstrip()
-        else:
-            return None
+        self.__get(['container', 'dockerid'])
 
     def set_container_id(self, new_id):
-        path = self.__get_container_file()
-
-        with open(path, "w") as f:
-            f.write(new_id)
-
-    def __get_container_file(self):
-        return "{}/container.dockerid".format(self.directory)
+        self.__set(['container', 'dockerid'], new_id)
 
     def get_image_id(self):
-        path = self.__get_image_file()
-
-        if os.path.isfile(path):
-            with open(path, "r") as f:
-                return f.read().rstrip()
-        else:
-            return None
+        self.__get(['image', 'dockerid'])
 
     def set_image_id(self, new_id):
-        path = self.__get_image_file()
+        self.__set(['image', 'dockerid'], new_id)
+
+    def __set(self, keys, value):
+        state = self.__get_state()
+
+        final_key = keys.pop()
+        final_dict = state
+        for key in keys:
+            if not key in final_dict:
+                final_dict[key] = {}
+            final_dict = final_dict[key]
+
+        final_dict[final_key] = value
+
+        self.__set_state(state)
+
+    def __get(self, keys):
+        state = self.__get_state()
+
+        final_key = keys.pop()
+        final_dict = state
+        for key in keys:
+            if not key in final_dict:
+                return None
+            final_dict = final_dict[key]
+
+        return final_dict.get(final_key, None)
+
+    def __set_state(self, new_state):
+        path = self.__get_state_path()
 
         with open(path, "w") as f:
-            f.write(new_id)
+            json.dump(new_state, f)
 
-    def __get_image_file(self):
-        return "{}/image.dockerid".format(self.directory)
+        self.state = new_state
+
+    def __get_state(self) -> dict:
+        if self.state is None:
+            self.state = self.__load_state()
+
+        return self.state
+
+    def __load_state(self) -> dict:
+        path = self.__get_state_path()
+
+        if not os.path.isfile(path):
+            return {}
+
+        with open(path, "r") as f:
+            return json.load(f)
+
+    def __get_state_path(self):
+        return "{}/state.json".format(self.directory)
